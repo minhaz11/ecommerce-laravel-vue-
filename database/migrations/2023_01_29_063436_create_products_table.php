d90cc679536fd47a603720be71d4cb5f6373595e
minhaz11 <minhaz217@gmail.com>
2023-01-29 15:29:06 +0600

    project setup and created migration

---
+++ database/migrations/2023_01_29_063436_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->string('image');
+            $table->string('image_mime');
+            $table->integer('image_size');
+            $table->longText('description');
+            $table->decimal('price', 8, 2);
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
